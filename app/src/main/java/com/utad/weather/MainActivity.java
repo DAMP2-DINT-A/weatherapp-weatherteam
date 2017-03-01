941a15e6afef75476d505bc77b8f47f911c2b373
Pablo Rojas <projascifre@gmail.com>
2017-03-01 12:25:36 +0100

    peticion openweahter lat lon

--- app/src/main/java/com/utad/weather/MainActivity.java
+++ app/src/main/java/com/utad/weather/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OpenWeatherManager op = new OpenWeatherManager();
+        op.getWeatherByLatLon(35,139);
     }
 }
--- app/src/main/java/com/utad/weather/OpenWeatherManager.java
+++ app/src/main/java/com/utad/weather/OpenWeatherManager.java
@@ -7,6 +7,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -14,23 +16,58 @@ import org.json.JSONObject;
  */
 public class OpenWeatherManager {
 
-    private String url =  "api.openweathermap.org/data/2.5/weather?";
+
+    private String url =  "http://api.openweathermap.org/data/2.5/weather?";
+
+    private String ApiKey = "&appid=2561671dd40fb9f79239772713fc07e9";
+
+    private static final String TAG= "OpenWeatherManagerApi";
 
 
+    public void getWeatherByLatLon (double lat, double lon){
+        url = url + "lat="+lat+"&lon="+lon+ApiKey;
+        Log.d("Query",""+ url);
+        JsonObjectRequest jsArrayRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray weather = response.getJSONArray("weather");
+                            for (int i = 0; i < weather.length(); i++){
+                                JSONObject obj = (JSONObject) weather.get(i);
+                                String descripcion = obj.get("description").toString();
+                                Log.d(TAG, "Descripcion " + descripcion);
+                            }
 
-    public void getWeatherByLatLon (Float lat, float lon){
-        url = url + "lat="+lat+"&lon="+lon;
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
-        
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "Error Respuesta en JSON: " + error.getMessage());
+
+                    }
+                }
+        );
     }
 
     public void getWeatherByZip(int zip, String countryCode){
-        url = url + "zip="+zip+","+countryCode;
+        url = url + "zip="+zip+","+countryCode+ApiKey;
+        Log.d("Query",""+ url);
 
     }
 
     public void getWeatherByName(String cityName){
-        url = url + "q="+cityName;
+        url = url + "q="+cityName+ApiKey;
+        Log.d("Query",""+ url);
     }
 
 
