f22e8e8517fb1404dadf370a69a64c70f420601d
Charlybiersack <carlos_lallama@hotmail.com>
2017-03-01 13:27:26 +0100

    Hecho

--- app/src/main/java/com/utad/weather/Flecha.java
+++ app/src/main/java/com/utad/weather/Flecha.java
@@ -3,16 +3,21 @@ package com.utad.weather;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * Created by carlos.marana on 01/03/2017.
  */
-public class Flecha extends ImageView {
+public class Flecha extends LinearLayout {
 
     ImageView ivFlecha;
     TextView tvVelocidad;
+    private Animation tD;
 
     public Flecha(Context context) {
         super(context);
@@ -29,13 +34,24 @@ public class Flecha extends ImageView {
         inicio();
     }
 
-    private void inicio(){
-        String infService = Context.LAYOUT_INFLATER_SERVICE;
-        LayoutInflater li = (LayoutInflater)getContext().getSystemService(infService);
-        li.inflate(R.layout.flecha, this, true);
 
-        ivFlecha = (ImageView)findViewById(R.id.ivFlecha);
-        tvVelocidad = (TextView)findViewById(R.id.tvVelocidad);
+    private void inicio() {
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li = (LayoutInflater) getContext().getSystemService(infService);
+        li.inflate(R.layout.flecha, this, true);
+        ivFlecha = (ImageView) findViewById(R.id.ivFlecha);
+        tvVelocidad = (TextView) findViewById(R.id.tvVelocidad);
+    }
+
+    public void setRotation(float posicion) {
+
+        ivFlecha.setRotation(posicion);
+    }
+
+    public void setVelocidad(float velocidad) {
+
+        tvVelocidad.setText(velocidad + " MPS");
+
     }
 
 }
